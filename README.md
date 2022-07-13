3b4e8feea65d4559f96e994791bc0a20acfa7ab6
illlights <illlights@163.com>
2022-07-13 15:11:39 +0800

    基本框架

---
+++ README.md
@@ -0,0 +1,5 @@
+# illlights API
+
+[api.illlights.com](https://api.illlights.com)
+
+目前还没有东西
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const https = require('https')
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/' function(req, res) {
+  res.sendFile('document.html');
+});
+
+
+
+
+app.listen(3000, fuction() {
+  console.log('app listening on port 3000');
+});

bca156cafb722a348ff270f2be0235ea9bb966a9
illlights <illlights@163.com>
2022-07-15 15:19:30 +0800

    美化document页面，区分中英文

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-// DB
+// Connect DB
 
 
 // Random Saying Page
@@ -32,8 +32,39 @@ app.get('/saying', function(req, res){
   });
 });
 
+app.get('/saying/:language', function(req, res) {
+  const params ={
+    language: req.params.language,
+    search: req.query.search,
+    type: req.query.type,
+    author: req.query.author,
+    from: req.query.from
+  };
 
-
+  if (params.language === 'zh') {
+    Saying.aggregate([ {$match: {language: {$exists: 0}}}, { $sample: { size: 1 } } ]).exec(function(err, result) {
+      if (err){
+        console.log(err);
+        res.send('内部错误，请重试或联系管理员');
+      } else{
+        if (JSON.stringify(result) == "[]") {
+          res.send('查询无结果');
+        } else { res.send(result); }
+      }
+    });
+  } else {
+    Saying.aggregate([ {$match: {language: params.language}}, { $sample: { size: 1 } } ]).exec(function(err, result) {
+      if (err){
+        console.log(err);
+        res.send('内部错误，请重试或联系管理员');
+      } else{
+        if (JSON.stringify(result) == "[]") {
+          res.send('查询无结果');
+        } else { res.send(result); }
+      }
+    });
+  }
+});
 
 
 
@@ -41,6 +72,9 @@ app.get('/saying', function(req, res){
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/document.html');
 });
+
+
+
 
 app.listen(3000, function() {
   console.log('app listening on port 3000');
